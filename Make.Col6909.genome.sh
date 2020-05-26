9ad8e31fb5b7cd09b535312c3baf8e7adcb34c8b
akihirao <akihirao@gmail.com>
2020-05-26 16:11:04 +0900

    minor update

--- Make.Col6909.genome.sh
+++ Make.Col6909.genome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -i
 #Make.Col6909.genome.sh
-#making genome fasta of Columbia ecotype (Col) deposited in the 1001 genomes
+#making genome fasta of Columbia ecotype (Col) deposited in the 1001 genomes: 
+#https://abrc.osu.edu/stocks/number/CS76778
 #by HIRAO Akira		
 
 set -exuo pipefail
